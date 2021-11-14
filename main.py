46d79b8ff0f49c96f679e48561c9d72b4e9402d1
ycwdaaaa <446051551@qq.com>
2021-11-14 11:24:08 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from selene.support.shared import browser, config
+from selenium.webdriver import Chrome
+import selenium.webdriver
+
+if __name__ == '__main__':
+    config.browser_name = 'chrome'
+    config.base_url = "http://k8s.testing-studio.com:5001"
+    config.timeout = 10
+    config.save_screenshot_on_failure = False
+
+    option = selenium.webdriver.ChromeOptions()
+    option.add_argument("--disable-infobars")
+    option.add_argument("--disable-dev-shm-usage")
+    option.add_argument("--no-sandbox")
+    option.add_argument("--disable-extensions")
+    option.add_argument("--ignore-ssl-errors")
+    option.add_argument("--ignore-certificate-errors")
+    option.add_argument('--disable-gpu')
+    prefs = {'download.default_directory': '/home/seluser/Downloads/'}
+    option.add_experimental_option('prefs', prefs)
+    option.add_experimental_option('w3c', False)
+    # option.add_experimental_option('perfLoggingPrefs', {
+    #     'enableNetwork': True,
+    #     'enablePage': False,
+    # })
+    caps = option.to_capabilities()
+    caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+    config.driver = selenium.webdriver.Remote(
+        command_executor="http://k8s.testing-studio.com:5001/wd/hub",
+        desired_capabilities=caps,
+        keep_alive=True,
+        options=option)
+    config.driver.set_page_load_timeout(10)
+
+browser.open('/')
+browser.driver.maximize_window()
